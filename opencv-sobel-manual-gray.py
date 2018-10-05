c4d0e8b12fab97f10d2fc64697fb4a671b55435f
dan <danilobcaval@gmail.com>
2018-10-04 23:29:54 -0300

    Updated README opencv-sobel-manual-gray.py now loads an image in grayscale, calculates Sobel X and Y images using the literature algorithm and displays them with plt.show() sobelCustom.py was added the function createEdgeImage with the intention of creating an edge-enhanced image from a given base. It uses sobelFilter or any other available 3x3 kernel convolution function

--- opencv-sobel-manual-gray.py
+++ opencv-sobel-manual-gray.py
@@ -1,10 +1,15 @@
+# Author: Danilo Cavalcanti
+# Importing System Modules
 import cv2 as cv
 import math as m
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Import personal implementation library
+from sobelCustom import *
+
 # path to img
-path = "redim.jpg"
+path = "./images/objects/101.pgm"
 img = cv.imread(path,cv.IMREAD_GRAYSCALE)
 
 
@@ -22,9 +27,25 @@ plt.yticks([])
 #plt.show()
 
 # This is where the processing begins
+x_img, y_img = createEdgeImage(np.array(img,np.float64))
+# Take absolute value
+x_img = np.absolute(x_img)
+y_img = np.absolute(y_img)
+# TODO normalize values to 0 - 255
 
-# Initialize empty matrix to receive Gradient
-newimg = np.array(np.zeros(img.shape) ,dtype='uint8')
+# Convert back to Grayscale
+x_img = np.uint8(x_img)
+y_img = np.uint8(y_img)
+
+# Plot side by side for comparison
+plt.subplot(1,3,1), plt.imshow(img,cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,3,2), plt.imshow(x_img,cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,3,3), plt.imshow(y_img,cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+
 
 
 import code
--- sobelCustom.py
+++ sobelCustom.py
@@ -13,7 +13,7 @@ sKernelY=np.array([[-1,-2,-1],
                       [0,0,0],
                       [1,2,1]],np.float64)
 
-def sobelCustom(img=-1):
+def sobelFilter(img=-1):
     '''Function that calculates Gx and Gy of a 3x3 img in numpy matrix form
     Arguments:
     - img: 3x3 region to process Gx and Gy
@@ -38,4 +38,45 @@ def sobelCustom(img=-1):
         
         return Gx,Gy
 
+def createEdgeImage(img=-1):
+    ''' Applies Sobel filter on a NxM image "img" loaded via OpenCV (cv2 package) and
+    returns three (N-2)x(M-2) images with sobelX, sobelY and both filters applied.
+    2 rows and 2 columns removed to simplify boundary conditions.
+    Arguments:
+    - img: Region in Grayscale color scheme and np.float64 format
+    '''
+    if(type(img) != np.ndarray):
+        print("Invalid 'img' parameter, returning empty matrix")
+        return np.array([0],np.float64)
+    elif(img.dtype != np.float64):
+        print("Invalid 'img' dtype (not float64), returning empty matrix")
+        return np.array([0],np.float64)
+    else:
+        # Create images ignoring last row and column for simplicity in
+        # convolution operation
+        x_image = np.zeros([img.shape[0]-2,img.shape[1]-2])
+        y_image = np.zeros([img.shape[0]-2,img.shape[1]-2])
+        # TODO normalize and do xy image
+        #xy_image = np.zeros([img.shape[0]-2,img.shape[1]-2])
+        for i in range(1,img.shape[0]-1):
+            for j in range(1,img.shape[1]-1):
+                #Initialize Gx & Gy for each pixel
+                Gx, Gy = 0, 0
+                # Get 3x3 submatrixes with np.ix_
+                # [i-1,i,i+1] = range(i-1,i+2)
+                # kept explicit for clarity
+                ixgrid = np.ix_([i-1,i,i+1],[j-1,j,j+1])
+                workingArea = img[ixgrid]
+                # Call the convolution function
+                Gx, Gy = sobelFilter(workingArea)
+                x_image[i-1][j-1] = Gx
+                y_image[i-1][j-1] = Gy
+                
 
+        return x_image, y_image
+    
+    
+    
+    
+    
+    
