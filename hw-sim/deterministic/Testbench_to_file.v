6767eb0545279f67a6e7685dd038f26a592ecfbf
danilo-bc <danilobcaval@gmail.com>
2019-03-19 22:09:03 -0300

    sobel3x3det module properly reads an 8-bit hex text file and operates on it. Next step is deciding how to normalize the results for the range 0-255 in a Verilog-friendly way

--- hw-sim/deterministic/Testbench_to_file.v
+++ hw-sim/deterministic/Testbench_to_file.v
@@ -1,27 +1,36 @@
 module Testbench_to_file();
-	parameter src_rows = 436;
-	parameter src_cols = 576;
+	//parameter src_rows = 436;
+	//parameter src_cols = 576;
+	parameter src_rows = 3;
+	parameter src_cols = 4;
 	parameter src_size = src_rows*src_cols;
+	//parameter edge_size = (src_rows-1)*(src_cols-1);
 	reg [7:0] src [0:src_size-1];
-	//reg [7:0] edges [0:217];
+	//reg [11:0] edges [0:edge_size-1];
 
 	// Variable to save file handle
 	integer f;
 	// Auxiliary variables for counting in loops
 	integer i,j;
 
-	wire [7:0] z [0:5];
-	wire [7:0] z_out; //binary value of output
+	reg [7:0] z1;
+	reg [7:0] z2;
+	reg [7:0] z3;
+	reg [7:0] z4;
+	reg [7:0] z5;
+	reg [7:0] z6;
+
+	wire [11:0] z_out; //binary value of output
 
 	reg clk;
 	reg reset;
 
-	sobel3x3det det1(.z1(z[0]),
-					 .z2(z[1]),
-					 .z3(z[2]),
-					 .z4(z[3]),
-					 .z5(z[4]),
-					 .z6(z[5]),
+	sobel3x3det det1(.z1(z1),
+					 .z2(z2),
+					 .z3(z3),
+					 .z4(z4),
+					 .z5(z5),
+					 .z6(z6),
 					 .z_out(z_out)
 					);
 
@@ -30,18 +39,38 @@ module Testbench_to_file();
 	end
 
 	initial begin
+		z1 = 8'h01;
+		z2 = 8'h00;
+		z3 = 8'h00;
+		z4 = 8'h00;
+		z5 = 8'h00;
+		z6 = 8'h00;
+
+		#2
 		//Load image coded in hexadecimal as memory
-		$display("z_out = %b",z_out);
+		$display("z_out = %x",z_out);
+
 		$display("Loading image into memory");
-		$readmemh("src.txt",src);
-		f = $fopen("src_after_load.txt","w");
-		for (i=0;i<src_rows;i=i+1)
+		$readmemh("teste.txt",src);
+		f = $fopen("teste_after_load.txt","w");
+		for (i=1;i<src_rows-1;i=i+1)
 		begin
-			for(j=0;j<src_cols-1;j=j+1)
+			for(j=1;j<src_cols-1;j=j+1)
 			begin
-				$fwrite(f,"%x ",src[src_cols*i+j]);
+				// Input for Gy
+				z1 = src[src_cols*(i-1)+j-1];
+				z2 = src[src_cols*(i-1)+j];
+				z3 = src[src_cols*(i-1)+j+1];
+				z4 = src[src_cols*(i+1)+j-1];
+				z5 = src[src_cols*(i+1)+j];
+				z6 = src[src_cols*(i+1)+j+1];
+
+				#2 // give an instant for z_out to update
+				//edges[src_cols*(i-1)+j-1]=z_out;
+				//$display("edges: %h",edges[src_cols*(i-1)+j-1]);
+				$fwrite(f,"%x ",z_out);
 			end
-			$fwrite(f,"%x\n",src[src_cols*i+src_cols-1]);
+			$fwrite(f,"\n");
 		end
 		$fclose(f);
 
--- hw-sim/deterministic/sobel3x3det.v
+++ hw-sim/deterministic/sobel3x3det.v
@@ -4,10 +4,20 @@ module sobel3x3det(input [7:0] z1,
 					input [7:0] z4,
 					input [7:0] z5,
 					input [7:0] z6,
-					output [7:0] z_out
+					output reg [11:0] z_out
 					);
+	reg [7:0] oper1;
+	reg [7:0] oper2;
+	always @(*) begin
+		oper1 = (z1+2*z2+z3);
+		oper2 = (z4+2*z5+z6);
+		if (oper1>oper2) begin
+			z_out = (oper1-oper2);
+		end
+		else begin
+			z_out = (oper2-oper1);
+		end
 
-
-	assign z_out = 8'b1;
+	end
 
 endmodule
