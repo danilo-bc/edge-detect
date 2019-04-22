0ac1ef4f84ccc42abe829d48708d17341f4defd3
danilo-bc <danilobcaval@gmail.com>
2019-04-22 15:26:28 -0300

    Made changes to hardware version to match software

--- hw-sim/deterministic/Testbench_to_file.v
+++ hw-sim/deterministic/Testbench_to_file.v
@@ -4,15 +4,12 @@ module Testbench_to_file();
 	parameter src_size = src_rows*src_cols;
 	parameter edge_size = (src_rows-1)*(src_cols-1);
 	reg [7:0] src [0:src_size-1];
-	reg [11:0] edges [0:edge_size-1];
+	reg [7:0] edges [0:edge_size-1];
 
 	// Variable to save file handle
 	integer f;
 	// Auxiliary variables for counting in loops
 	integer i,j;
-
-	// Auxiliary variable to normalize for 8-bit image
-	integer biggest;
 
 	// Registers and wires for the Sobel module
 	reg [7:0] z1;
@@ -25,7 +22,7 @@ module Testbench_to_file();
 	reg [7:0] z8;
 	reg [7:0] z9;
 
-	wire [11:0] z_out;
+	wire [7:0] z_out;
 
 	// Auxiliary system signals
 	// May be used in the future
@@ -61,14 +58,12 @@ module Testbench_to_file();
 		z7 = 8'h00;
 		z8 = 8'h00;
 		z9 = 8'h00;
-		// Initialize 'biggest' auxiliary variable
-		biggest = 0;
 
 		#2
 		//Load image coded in hexadecimal as memory
 		$display("Loading image into memory");
 		$readmemh("src.txt",src);
-
+		f = $fopen("edges_hw_det.txt","w");
 		// 'Crop' area around central pixel z5
 		for (i=1;i<src_rows-1;i=i+1) begin
 			for(j=1;j<src_cols-1;j=j+1) begin
@@ -86,22 +81,12 @@ module Testbench_to_file();
 				z9 = src[src_cols*(i+1)+j+1];
 
 				#2 // give an instant for z_out to update
-				// Update biggest number to normalize later
-				if(z_out > biggest) begin
-					biggest = z_out;
-				end
+				//save 'edges' array for possible future use
 				edges[src_cols*(i-1)+j-1]=z_out;
+				//Write on file
+				$fwrite(f,"%x ",z_out);
 			end
-
-		end
-		f = $fopen("edges_hw_det.txt","w");
-		// Normalize to 8 bits and write to file
-		for (i=0;i<src_rows-2;i=i+1) begin
-			for(j=0;j<src_cols-2;j=j+1) begin
-				edges[src_cols*(i)+j] = (edges[src_cols*(i)+j]*255)/biggest;
-				$fwrite(f,"%x ",edges[src_cols*(i)+j][7:0]);
-			end
-			$fwrite(f,"\n");
+		$fwrite(f,"\n");
 		end
 		$fclose(f);
 
--- hw-sim/deterministic/sobel3x3det.v
+++ hw-sim/deterministic/sobel3x3det.v
@@ -7,13 +7,38 @@ module sobel3x3det(input [7:0] z1,
 					input [7:0] z7,
 					input [7:0] z8,
 					input [7:0] z9,
-					output reg [11:0] z_out
+					output reg [7:0] z_out
 					);
 	wire [11:0] z_x;
 	wire [11:0] z_y;
 
+	reg [8:0] aux_x;
+	reg [8:0] aux_y;
+
 	always @(*) begin
-		z_out = z_x+z_y;
+	/*
+	This block uses the same normalization strategy
+	of the software counterpart. Saturate values up to
+	8-bits (255), copy the rest, then sum 0.5x+0.5y
+	z_x and z_y are already in absolute value
+	*/
+		if(z_x > 12'd255) begin
+			aux_x = 9'd255;
+		end
+		else begin
+			aux_x = {1'b0,z_x[7:0]};
+		end
+
+		if(z_y > 12'd255) begin
+			aux_y = 9'd255;
+		end
+		else begin
+			aux_y = {1'b0,z_y[7:0]};
+		end
+
+		// Weighted sum
+		z_out = (aux_x+aux_y)>>1;
+		
 	end
 
 	sobel3x3det_x sobel_x(.z1(z1),
