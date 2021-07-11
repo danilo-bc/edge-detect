e2542a3071388591d873905080a062f4277eeec6
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-07-11 12:27:49 -0300

    Cleanup of processAll script. It is meant to be adjusted for the user's needs

--- sw-sim/processAll.py
+++ sw-sim/processAll.py
@@ -2,82 +2,50 @@ import det.detSobel as ds
 import stoch.stochWrapper as sw
 import glob
 import numpy as np
-import ray
 
-ray.init()
+fileList = glob.glob("DEFINE YOUR PATH TO PICTURES TO PROCESS HERE")
 
-fileList = glob.glob("/mnt/hdd1tera/databaseDanilo/IEEE-CURE-OR/10_grayscale_no_challenge/texture1/LG/*.jpg")
-
-print("Parte deterministica")
+print("Deterministic processing")
 for file in fileList:
-   print("Processando: ",file)
+   print("Processing: ",file)
    src,edges_det = ds.detectAndShow(file,errRate=0.00,show=False)
    sw.saveHex("./results/det/noErr/"+file.split('/')[-1][:-4]+".txt",edges_det)
 
-print("Parte estocastica")
+print("Stochastic processing")
 for file in fileList:
-   print("Processando: ",file)
-   src,edges_stoch = sw.rayDetectAndShow(file,errRate=0.00,show=False)
-   sw.saveHex("./results/stoch/NoErr/"+file.split('/')[-1][:-4]+".txt",edges_stoch)
-   # sw.saveHex("./results/stoch/errRate/"+file.split('/')[-1][:-4]+"0_05.txt",edges_stoch)
+   print("Processing: ",file)
+   src,edges_stoch = sw.detectAndShow(file,errRate=0.00,show=False)
+   sw.saveHex("./results/stoch/noErr/"+file.split('/')[-1][:-4]+".txt",edges_stoch)
 
+## Calculate metrics and write in files
+for proc_type in ('det', 'stoch'):
+   resultList = glob.glob("./results/"+proc_type+"/*.txt")
+   resultList = list(np.sort(resultList))
+   meanAbsError = []
+   meanSquaredError = []
+   for errRate in ('0_01', '0_02', '0_05'):
+      for result in resultList:
+         reference = np.float64(sw.loadHex(result))
+         afterError = np.float64(sw.loadHex(result.replace(proc_type,""+proc_type+"/errRate/"+errRate)))
+         meanAbsError.append(np.abs(np.subtract(reference,afterError)).mean())
+         meanSquaredError.append(np.square(np.subtract(reference,afterError)).mean())
 
-# resultList = glob.glob("./results/det/*.txt")
-# resultList = list(np.sort(resultList))
-# meanAbsError = []
-# for result in resultList:
-#    detR = np.float64(sw.loadHex(result))
-#    stochR = np.float64(sw.loadHex(result.replace("det","stoch")))
-#    meanAbsError.append(np.abs(np.subtract(detR,stochR)).mean())
+      for errMetric in ('MAE', 'MSE') :
+         f = open("./results/result"+proc_type+errRate+errMetric".txt","w")
 
-# resultList = glob.glob("./results/det/*.txt")
-# resultList = list(np.sort(resultList))
-# meanAbsError = []
-# meanSquaredError = []
-# for result in resultList:
-#    reference = np.float64(sw.loadHex(result))
-#    afterError = np.float64(sw.loadHex(result.replace("det","det/errRate/0_05")))
-#    meanAbsError.append(np.abs(np.subtract(reference,afterError)).mean())
-#    meanSquaredError.append(np.square(np.subtract(reference,afterError)).mean())
+         f.write(errMetric+":\n")
+         if errMetric == 'MAE':
+            for r in meanAbsError:
+               f.write(str(r)+"\n")
+            f.write("Mean:\n" + str(np.mean(meanAbsError))+"\n")
 
+         if errMetric == 'MSE':
+            f.write("MSE:\n")
+            for r in meanSquaredError:
+               f.write(str(r)+"\n")
+            f.write("Mean:\n" + str(np.mean(meanSquaredError))+"\n")
 
-# f = open("./results/resultDet0_05.txt","w")
-
-# f.write("MAE:\n")
-# for r in meanAbsError:
-#    f.write(str(r)+"\n")
-# f.write("Média:\n" + str(np.mean(meanAbsError))+"\n")
-
-# f.write("MSE:\n")
-# for r in meanSquaredError:
-#    f.write(str(r)+"\n")
-# f.write("Média:\n" + str(np.mean(meanSquaredError))+"\n")
-# f.close()
-
-# resultList = glob.glob("./results/stoch/*.txt")
-# resultList = list(np.sort(resultList))
-# meanAbsError = []
-# meanSquaredError = []
-# for result in resultList:
-#    reference = np.float64(sw.loadHex(result))
-#    afterError = np.float64(sw.loadHex(result.replace("stoch","stoch/errRate/0_05")))
-#    meanAbsError.append(np.abs(np.subtract(reference,afterError)).mean())
-#    meanSquaredError.append(np.square(np.subtract(reference,afterError)).mean())
-
-
-# f = open("./results/resultStoch0_05.txt","w")
-# f.write("MAE:\n")
-# for r in meanAbsError:
-#    f.write(str(r)+"\n")
-# f.write("Média:\n" + str(np.mean(meanAbsError))+"\n")
-
-# f.write("MSE:\n")
-# for r in meanSquaredError:
-#    f.write(str(r)+"\n")
-# f.write("Média:\n" + str(np.mean(meanSquaredError))+"\n")
-
-# f.close()
-
+         f.close()
 
 # import code
 # code.interact(local=locals())
