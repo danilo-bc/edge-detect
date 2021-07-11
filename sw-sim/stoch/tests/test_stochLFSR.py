2ebc6bfa57fa3d5f8ad944677af38fc1dcf135f6
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-07-11 12:12:43 -0300

    Hard-coded expected LFSR sequences. They come from hw-sim/LFSR-tb. The proper way to test this would be some sort of co-simulation python+verilog, but it takes way less time to simply run LFSR-tb and check the sequences visually

--- sw-sim/stoch/tests/test_stochLFSR.py
+++ sw-sim/stoch/tests/test_stochLFSR.py
@@ -6,27 +6,26 @@ from bitarray import bitarray
 
 class Test_stochLFSR_behaviour(unittest.TestCase):
     def test_4_bit_lfsr(self):
-        oracle = open("lfsr4bOracle.data",'r')
+        # Reference sequence from hw-sim/LFSR-tb
+        expected_sequence = ['0000','0001','0010','0100','1001','0011','0110','1101','1010','0101','1011','0111','1111','1110','1100','1000']
         seed=('1000')
         lfsr_reg = bitarray(seed)
 
         for i in range(16):
-            expected = bitarray(oracle.readline()[:-1])
+            expected = bitarray(expected_sequence[i])
             lfsr_reg = lfsr.shift(lfsr_reg)
             self.assertEqual(expected, lfsr_reg)
-        oracle.close()
 
     def test_8_bit_lfsr(self):
         seed=('10000000')
-        oracle = open("lfsr8bOracle.data",'r')
+        expected_sequence = ['00000000','00000001','00000011','00000111','00001111','00011111','00111111','01111110','11111100','11111001','11110010','11100101','11001010','10010100','00101001','01010010']
         lfsr_reg = bitarray(seed)
 
         for i in range(16):
-            expected = bitarray(oracle.readline()[:-1])
+            expected = bitarray(expected_sequence[i])
             lfsr_reg = lfsr.shift(lfsr_reg)
             print(i, lfsr_reg)
             self.assertEqual(expected, lfsr_reg)
-        oracle.close()
 
 if __name__=='__main__':
     unittest.main()
